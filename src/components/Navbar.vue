0a89c17c652898cff9ca1d1390febe15e1c977d9
路明 <snaillu@yeah.net>
2020-03-29 21:31:31 +0800

    新增底部nav组件

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="navbar">
+        <ul class="flex">
+            <li class="navbar-item flex-item-1" v-for="(item,index) in navbarList" :key="index">{{item}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        data: function(){
+            return {
+                navbarList: ['首页','书影音','小组','市集','我的'],
+            }
+        },
+        props: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .navbar{
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 2rem;
+        line-height: 2rem;
+        border-top: 1px solid #eee;
+        .navbar-item{
+            font-size: 1rem;
+            transform: scale(0.5);
+            -webkit-transform: scale(0.5);
+            // zoom: 0.5;
+            transform-origin: center;
+        }
+    }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar';
+export default {
+  name: 'Home',
+  data: function(){
+    return {
+      
+    }
+  },
+  props: {
+    msg: String
+  },
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.home{
+
+}
+</style>
